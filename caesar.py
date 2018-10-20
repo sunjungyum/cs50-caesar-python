d09c252f84c643116de849b9b6e14f1019a35593
sunjungyum <sunjungyum@users.noreply.github.com>
2018-10-20 21:31:28 +0000

    20181020T213118Z

--- caesar.py
+++ caesar.py
@@ -17,19 +17,22 @@ plaintext = get_string("plaintext: ")
 print("ciphertext: ", end="")
 
 # Iterates through each character of the plaintext
-for c in plaintext:
-    if c.isalpha():
+for p in plaintext:
+    if p.isalpha():
         # Rotates characters as uppercase characters
-        upper = c.upper()
+        upper = p.upper()
         alphabeticalindex = ord(upper) - 65
         # Uses a separate variable called "result" because c's case needs to be checked
         result = 65 + ((alphabeticalindex + key) % 26)
-
-        # Converts it back to a char (if original was lowercase, returns back to lowercase)
-        if c.islower():
+        # Converts it back to a separate char (if original was lowercase, returns back to lowercase)
+        if p.islower():
             c = chr(result).lower()
         else:
             c = chr(result)
+
+    # If char is not alphabetical, it remains the same
+    else:
+        c = p
 
     # Prints either the alphabetical rotated character or the nonalphabetical character as is
     print(c, end="")
